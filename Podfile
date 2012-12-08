052fc41a16edd98198824d201f692fc0e10e4ccc
mtgto <hogerappa@gmail.com>
2012-12-09 01:53:47 +0900

    add podfile

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '5.0'
+
+pod 'AFNetworking',  '~> 1.0.1'
+pod 'CocoaLumberjack', '~> 1.6'
+
+target :Tests, :exclusive => true do
+  pod 'GHUnitIOS', '~> 0.5.5'
+  pod 'OCMock', '~> 2.0.1'
+end
+
--- Tests/main.m
+++ Tests/main.m
@@ -2,17 +2,15 @@
 //  main.m
 //  Tests
 //
-//  Created by User on 12/9/12.
-//  Copyright (c) 2012 mtgto. All rights reserved.
+//  Created by <ユーザ名> on 12/8/12.
+//  Copyright (c) 2012 <組織名>. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-#import "AppDelegate.h"
-
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
     }
 }
