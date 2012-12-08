2156f4ff5835a999837c5915be8f5dd867caadf1
mtgto <hogerappa@gmail.com>
2012-12-09 01:45:39 +0900

    Initial Commit

---
+++ CocoaPodsSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaPodsSample
+//
+//  Created by User on 12/9/12.
+//  Copyright (c) 2012 mtgto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CocoaPodsSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CocoaPodsSample
+//
+//  Created by User on 12/9/12.
+//  Copyright (c) 2012 mtgto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
